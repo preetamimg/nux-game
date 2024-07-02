491ff951f743051575fbc3a63fdfb944d06ffb36
shivtejimg <shivtej.img@gmail.com>
2024-07-02 13:26:25 +0530

    project staup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'theme1': '#440091',
+        'theme2': '#7c7cfe',
+      },
+    },
+  },
+  plugins: [],
+}
